622c9372843bae95349382daab0770faa231033b
thiago <thiagotsdelima@gmail.com>
2024-01-25 15:09:33 -0300

    update Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,42 @@
-import { Container } from './styles';
+import { Container, Profile, SearchContainer } from './styles';
+import { Link } from 'react-router-dom';
+import { Input } from '../../components/Input';
+import { HiMagnifyingGlass } from "react-icons/hi"; 
+import { IoLogOutOutline } from "react-icons/io5";
+import { FaBars } from "react-icons/fa";
+import { PiReceiptThin } from "react-icons/pi";
 
-export function Header() {
-  return(
+export function Header({ onChange }) {
+  return (
     <Container>
+       <MobileIcon>
+       <FaBars />
+      </MobileIcon>
+      <Link to="/">
+        <img src="/public/Polygon1.svg" alt="Logo" /> 
+        <h1>FoodExplorer</h1>
+      </Link>
+      <MobileStar>
+      <PiReceiptThin />
+      </MobileStar>
+
+      <SearchContainer> 
+        <HiMagnifyingGlass />
+        <Input
+          placeholder="Busque por pratos ou ingredientes"
+          onChange={onChange}
+        />
+      </SearchContainer>
       
+      <Profile>
+        <div>
+          <Button title="Pedidos" />
+        </div>
+        
+        <Link title="Sair" to="/">
+          <IoLogOutOutline />
+        </Link>
+      </Profile>
     </Container>
-  )
+  );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -9,9 +9,60 @@ display:flex;
 justify-content: space-between;
 padding: 0 11.3rem;
 
-@media (min-width: 360px) {
+.searchContainer {
+  display: flex;
+  align-items: center;
+}
+
+@media (min-width: 768px) { 
+  .MobileIcon, .MobileStar {
+    display: none;
+  }
+}
+
+
+@media (max-width: 767px) {
+  .MobileIcon, .MobileStar {
+    display: block; 
+  }
+}
+
+@media (min-width: 768px) {
+    padding: 0 2rem; 
+    height: auto; 
+Input {
+    display: none;
+}
+  }
+@media (min-width: 370px) {
 grid-area: header;
 height: 11.4rem;
 width: 100%;
+Input {
+    display: none;
+}
 }
 `;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 11px;
+
+  img {
+    width: 64px;
+    height: 64px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    line-height: 18px;
+
+    button {
+      font-size: 14px;
+      font-weight: 400;
+      color: ${({ theme }) => theme.COLORS.TOMATO_100};
+    }
+  }`;

942dbc39b84583a52f0d22a544eac2b7ae9820c6
thiago <thiagotsdelima@gmail.com>
2024-02-06 16:10:13 -0300

    update header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,59 +1,88 @@
 import { Container, Profile, MobileIcon, MobileStar, Found, InputContainer } from './styles';
-
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button } from '../Button';
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaBars } from 'react-icons/fa';
+import { USER_ROLES } from "../../utils/roles"
+import { api } from "../../services"
 import { useAuth } from '../../hooks/auth';
-import { FaBars } from "react-icons/fa";
-
 
 export function Header({ onChange }) {
-  const { signOut } = useAuth();
-  
-  function handleSignOut(){
+  const { user, signOut } = useAuth();
+  const navigate = useNavigate();
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+
+  function handleSignOut() {
     signOut();
+    navigate(-1);
   }
-  
+
+  function handleAddNewDish() {
+    navigate('/adminAddMeals'); 
+  }
+
   return (
     <Container>
+      <MobileIcon>
+        <FaBars />
+      </MobileIcon>
 
-        <MobileIcon>
-          <FaBars />
-        </MobileIcon>
+      {toggleMenu && (
+        <nav>
+          <ul>
+            {USER_ROLES.ADMIN.includes(user?.role) ? (
+              <>
+                <li>
+                  <button onClick={handleAddNewDish}>Novo Prato</button>
+                </li>
+              </>
+            ) : null}
+            <li>
+              <button onClick={handleSignOut}>Sair</button>
+            </li>
+          </ul>
+        </nav>
+      )}
 
       <Found>
-
-        <img src="/Polygon1.svg" alt="Logo" /> 
+        <img src="/Polygon1.svg" alt="Logo" />
         <h1>food explorer</h1>
-
+        {userType === 'admin' && <p>admin</p>}
       </Found>
-      <MobileStar>
+
+      <MobileStar onClick={() => setToggleMenu(!toggleMenu)}>
+       
         <img src="/Frame.png" alt="image of a torn sheet" />
+        
       </MobileStar>
 
       <InputContainer>
         <FaSearch className="inputIcon" />
         <input
-          type="text"
-          placeholder="Busque por pratos ou ingredientes"
-          onChange={onChange}
+        type="text"
+        placeholder="Busque por pratos ou ingredientes"
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
+        onKeyPress={(e) => e.key === 'Enter' && onChange(searchValue)}
         />
       </InputContainer>
 
       <Profile>
-
-        <div>
-        <Button title="Pedidos (0)">
-        <img src="/sheet.svg" alt="image of a torn sheet" />
-        </Button>
-        </div>
-        
-        <Link title="Sair" onClick={handleSignOut}>
+      {USER_ROLES.ADMIN.includes(user?.role) ?(
+      
+          <Button title="Novo Prato" onClick={handleAddNewDish}>
+            Novo Prato
+          </Button>
+        ) : (
+          
+          <Button title="Pedidos (0)">
+            <img src="/sheet.svg" alt="image of a torn sheet" />
+          </Button>
+        )}
+        <Link to="/" title="Sair" onClick={handleSignOut}>
           <img src="/arrow.png" alt="right arrow" />
         </Link>
-        
       </Profile>
-      
     </Container>
   );
 }

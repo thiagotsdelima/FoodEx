a1dc269ac6dcd570a02e409377b8c919c249832c
thiago <thiagotsdelima@gmail.com>
2024-02-22 21:59:48 -0300

    update add Meal

--- src/pages/AddMeal/index.jsx
+++ src/pages/AddMeal/index.jsx
@@ -1,4 +1,4 @@
-import { Container } from './styles';
+import { Container, Content, Main, Form } from './styles';
 import { useState } from 'react';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
@@ -16,76 +16,78 @@ export function AddMeal() {
 return (
 <Container>
   <Header />
-    <main>
+  <Content>
+    <Main>
+   
     <ButtonBack />
     <Section title='Adicionar prato' />
-    <form id="newDish" action="#" method="post" className="dishes">
-    <div className="formImage">
-    <label htmlFor="image" >
+    <Form id="newDish" action="#" method="post" className="dishes">
+  <div className="formRow">
+    <div className="formImageUpload">
       <p id="imageInput">Imagem do Prato</p>
-        <p className="upload">
-           <FiLogOut className="formIcon"/>
-            <p className="uploadText ">Selecione imagem</p>
-            <Input type="file" id="image" name="image" accept="image/*" onChange={(event)=>setDishImage(event.target.files[0])} />
-              </p>                   
-          </label>
-    
-
-    <div className="formInputs">
-      <label htmlFor="name">Nome</label>
-        <Input type="text" id="name" name="name" placeholder="Ex: Salada Ceasar" required onChange={(event)=>setName(event.target.value)}/>
+      <p className="upload">
+        <FiLogOut className="formIcon"/>
+        <span className="uploadText">Selecione imagem</span>
+        <Input type="file" id="image" name="image" accept="image/*" onChange={(event)=>setDishImage(event.target.files[0])} />
+      </p>
     </div>
 
     <div className="formInputs">
-        <label htmlFor="category">Categoria</label>
-         <div className="customSelect">
-         <select id="DrinkEatCategory" name="DrinkEatCategory" placeholder="Refeição" required onChange={(event)=>setCategory(event.target.value)}>
+      <label htmlFor="name">Nome</label>
+      <Input type="text" id="name" name="name" placeholder="Ex: Salada Ceasar" required onChange={(event)=>setName(event.target.value)}/>
+    </div>
+
+    <div className="formInputs">
+      <label htmlFor="category">Categoria</label>
+      <div className="customSelect">
+        <select id="DrinkEatCategory" name="DrinkEatCategory" required onChange={(event)=>setCategory(event.target.value)}>
           <option value="" disabled selected hidden>Selecione uma categoria</option>
           <option value="refeicao">Refeição</option>
           <option value="sobremesa">Sobremesa</option>
           <option value="bebida">Bebida</option>
         </select>
-        </div>
-        </div>
-        </div>
+      </div>
+    </div>
+  </div>
 
         
-        <div className="formInputs">
-        <label htmlFor="ingredients" id="seasoningLabel">
-        Ingredientes
-        <fieldset id="tagBackground" >                
-                    <Tag className="tag" />                
-                    <Tag className="tag" $isNew placeholder="Adicionar" required />                
-                  </fieldset>              
-        </label>
+        <div className="formRow">
+    <div className="formInputs">
+      <label htmlFor="ingredients">Ingredientes</label>
+      <fieldset id="tagBackground">                
+        <Tag className="tag" />                
+        <Tag className="tag" $isNew placeholder="Adicionar" required />                
+      </fieldset>              
+    </div>
         
         
 
         
-              <div className="formInputs">
-                <label htmlFor="price">Preço</label>
-                <Input type="text" id="price" name="price" placeholder="R$ 00,00" required onChange={(event)=>setPrice(event.target.value)}/>
-              </div>
-
-              </div>
+    <div className="formInputs">
+      <label htmlFor="price">Preço</label>
+      <Input type="text" id="price" name="price" placeholder="R$ 00,00" required onChange={(event)=>setPrice(event.target.value)}/>
+    </div>
+  </div>
               
-              <div className="formInputs">
+  <div className="formInputs">
                 <label htmlFor="description">Descrição</label>
                 <TextArea id="description" name="description" readOnly={false} placeholder="Fale brevemente sobre o prato, seus ingredientes e composição" rows="8" required onChange={(event)=>setDescription(event.target.value)} ></TextArea>
               </div>
               
-              <Button
+           
+             
+    </Form>
+    <Button
             type="submit" 
             className="submitButton"
             form="newDishForm"
             title="Salvar Informações"
             
           /> 
-             
-    </form>
-   
-    </main>
+         
+    </Main>
   <Footer />
+  </Content>
 </Container>
 );
 }
--- src/pages/AddMeal/styles.js
+++ src/pages/AddMeal/styles.js
@@ -1,12 +1,133 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-width: 100%;
-height: 100vh;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    position: relative;
+
+`;
+
+export const Content = styled.div`
+
+
+`;
+
+export const Main = styled.main`
+  flex-grow: 1;
+  padding: 0 2rem; /* Ajuste conforme necessário */
+  margin: 3.2rem auto 0;
+  max-width: 1120px;
+  .contentArea {
+   
+    
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 3rem;
+
+  .formRow {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1rem;
+
+    .formInputs {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      &:not(:last-child) {
+        margin-right: 1rem; 
+      }
+    }
+  }
+
+  .upload {
+    height: 3rem;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-top: 1.5rem;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+    border-radius: 0.5rem;
+
+    .formIcon {
+      margin: 0.75rem 0.5rem 0.75rem 1rem;
+      width: 2.5rem;
+      height: 2.5rem;
+      transform: rotate(-90deg);
+      cursor: pointer;
+    }
+  }
+  
+  select {
+      width: 100%;
+     
+      font-family: ${({ theme }) => theme.FONTS.RobotoSmallRegular};
+      
+      height: 3rem;
+      padding: 0.75rem 0.875rem;
+      border: none;
+      border-radius: 0.5rem;     
+      background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};    
+      color: ${({ theme }) => theme.COLORS.GRAY_500};      
+    }  
+    .uploadText {
+    font-family: ${({ theme }) => theme.FONTS.Poppins100Medium};  
+    color: ${({ theme }) => theme.COLORS.GRAY_100};  
+    line-height: 1.5rem;
+    white-space: nowrap;
+    cursor: pointer;
+  }
+  #image{
+    display: none;
+  }
+    #seasoningLabel{
+      font-size: 1rem; 
+
+      fieldset {
+        width: 22.75rem; 
+        height: 3rem;
+        gap: 1rem;
+        display: flex;
+        align-items: center;
+        border-radius: 0.5rem;  
+        border: none;      
+        margin-top: 1rem;
+        background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+        flex-wrap: wrap;
+      }    
+    }   
+    #description{
+      display: flex;
+      height: 10.75rem;
+      padding: 0.875rem;
+      align-items: flex-start;
+      gap: 0.875rem;
+      align-self: stretch;
+      border: none;
+      border-radius: 0.5rem;
+      background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800}; 
+    }
 
  
+    .submitButton{
+    margin: 1.5rem 3.2rem 8.13rem;
+    background-color: ${({ theme }) => theme.COLORS.TOMATO_400};
+  }
 
-main{
+  .submitButton:hover{
+    background-color: ${({ theme }) => theme.COLORS.TOMATO_100};
+  }
+`;
+
+
+/*main{
   
 
   
@@ -133,7 +254,4 @@ main{
   .submitButton:hover{
     background-color: ${({ theme }) => theme.COLORS.TOMATO_100};
   }
-
-`;
-
-
+*/

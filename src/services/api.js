fe4eda5ab7ebe41329d30a26a2636dd407405b1a
thiago <thiagotsdelima@gmail.com>
2024-03-18 17:07:34 -0300

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333/' /*https://backfoodexplorer.onrender.com */
+  baseURL: 'https://backfoodexplorer.onrender.com' 
 });
